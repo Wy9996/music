1c7d86088df4e660766ef0f1011dea119132d0a6
keyang <19115202436@163.com>
2021-01-07 21:40:15 +0800

    内网穿透

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,7 @@ Page({
   data: {
     bannerList:[],
     recommendList:[],
+    topList:[],
   },
 
   /**
@@ -18,11 +19,28 @@ Page({
   this.setData({
     bannerList:bannerListData.banners
   })
-  //内容数据
+  //获取推荐歌单数据
   let recommendListData = await request('/personalized',{limit: 10});
   this.setData({
     recommendList:recommendListData.result
   })
+  //获取排行榜数据
+  // idx的取值范围是0-20，需要0-4，发生五次请求
+  let index = 0;
+  let resultArr = [];
+  while(index <5){
+    let topListData = await request('/top/list',{idx: index++});
+    let topListItem = {name:topListData.playlist.name,tracks:topListData.playlist.tracks.slice(0,3)}
+    resultArr.push(topListItem);
+    //放在此处不需要循环请求结束才更新，用户体验好，但渲染次数会多一些
+    this.setData({
+      topList:resultArr
+    })
+  }
+  //更新值 放在此处会导致发生请求的时间过长，页面长时间白屏
+  // this.setData({
+  //   topList:resultArr
+  // })
   },
 
   /**
--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,5 @@
 // 配置服务器信息
 export default {
-  host:'http://localhost:3000'
+  host:'http://localhost:3000',
+  nwhost:'https://tmm9996.cn1.utools.club'
 }
